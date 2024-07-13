239322ed879349f7bb35e7bc674e69df8e96a5d0
abdullahiqbal93 <163277735+abdullahiqbal93@users.noreply.github.com>
2024-07-13 16:08:39 +0530

    new updated

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
 const header = document.querySelector('.header.container');
 
+
 hamburger.addEventListener('click', () => {
 	hamburger.classList.toggle('active');
 	mobile_menu.classList.toggle('active');
